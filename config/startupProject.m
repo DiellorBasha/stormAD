3916763953be967a2d4c83014ba7fdca4bb59d8d
Diellor Basha <49167439+DiellorBasha@users.noreply.github.com>
2024-07-09 12:51:21 -0400

    Refactored to startupProj

---
+++ config/startupProject.m
@@ -0,0 +1,12 @@
+fproj = matlab.project.rootProject;
+openGraphConn
+
+% Add Brainstorm to search path
+setenv('BRAINSTORM_DIR', 'C:\Users\diell\Documents\brainstorm3') 
+addpath(genpath(getenv('BRAINSTORM_DIR')));
+    
+% to do
+    % environment variables
+    % PAD data location
+
+  
