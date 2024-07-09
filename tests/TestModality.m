f74e58c1959b34833c3fdd6283a56c18fe13107e
Diellor Basha <49167439+DiellorBasha@users.noreply.github.com>
2024-07-09 13:01:12 -0400

    Added parametric testing

--- tests/TestModality.m
+++ tests/TestModality.m
@@ -1,44 +1,54 @@
 classdef TestModality < TestBase
+    properties (TestParameter)
+        modalityName = {'MRI', 'fMRI', 'PET'};
+        updatedModalityName = {'Updated MRI', 'Updated fMRI', 'Updated PET'};
+    end
+    
+    properties (MethodSetupParameter)
+        modalityId = {'modality_id_test1', 'modality_id_test2', 'modality_id_test3'};
+        datasetId = {'dataset_id_test1', 'dataset_id_test2', 'dataset_id_test3'};
+    end
+
     methods(TestMethodSetup)
-        function setup(testCase)
+        function setup(testCase, datasetId, modalityId)
             % Ensure the node does not exist before each test
-            executeCypher(testCase.graphconn, 'MATCH (m:Modality {id: "modality_id_test"}) DETACH DELETE m');
-            executeCypher(testCase.graphconn, 'MATCH (d:Dataset {id: "dataset_id_test"}) DETACH DELETE d');
+            executeCypher(testCase.graphconn, sprintf('MATCH (m:Modality {id: "%s"}) DETACH DELETE m', modalityId));
+            executeCypher(testCase.graphconn, sprintf('MATCH (d:Dataset {id: "%s"}) DETACH DELETE d', datasetId));
             
             % Create a dataset node for testing
-            dataset = Dataset(testCase.graphconn, 'dataset_id_test', 'Brain Health Study');
+            dataset = Dataset(testCase.graphconn, datasetId, 'Brain Health Study');
             dataset.create();
         end
     end
     
     methods(Test)
-        function testCreateModality(testCase)
-            modality = Modality(testCase.graphconn, 'modality_id_test', 'dataset_id_test', 'MRI');
+        function testCreateModality(testCase, modalityId, datasetId, modalityName)
+            modality = Modality(testCase.graphconn, modalityId, datasetId, modalityName);
             modality.create();
-            result = executeCypher(testCase.graphconn, 'MATCH (m:Modality {id: "modality_id_test"}) RETURN m');
+            result = executeCypher(testCase.graphconn, sprintf('MATCH (m:Modality {id: "%s"}) RETURN m', modalityId));
             testCase.verifyNotEmpty(result.m, 'Modality node was not created.');
         end
         
-        function testReadModality(testCase)
-            modality = Modality(testCase.graphconn, 'modality_id_test', 'dataset_id_test', 'MRI');
+        function testReadModality(testCase, modalityId, datasetId, modalityName)
+            modality = Modality(testCase.graphconn, modalityId, datasetId, modalityName);
             modality.create();
-            modalityNode = modality.read('modality_id_test');
-            testCase.verifyEqual(modalityNode.n.name, 'MRI', 'Modality name does not match.');
+            modalityNode = modality.read(modalityId);
+            testCase.verifyEqual(modalityNode.n.name, modalityName, 'Modality name does not match.');
         end
         
-        function testUpdateModality(testCase)
-            modality = Modality(testCase.graphconn, 'modality_id_test', 'dataset_id_test', 'MRI');
+        function testUpdateModality(testCase, modalityId, datasetId, modalityName, updatedModalityName)
+            modality = Modality(testCase.graphconn, modalityId, datasetId, modalityName);
             modality.create();
-            modality.update('modality_id_test', struct('name', 'Updated MRI'));
-            modalityNode = modality.read('modality_id_test');
-            testCase.verifyEqual(modalityNode.n.name, 'Updated MRI', 'Modality name was not updated.');
+            modality.update(modalityId, struct('name', updatedModalityName));
+            modalityNode = modality.read(modalityId);
+            testCase.verifyEqual(modalityNode.n.name, updatedModalityName, 'Modality name was not updated.');
         end
         
-        function testDeleteModality(testCase)
-            modality = Modality(testCase.graphconn, 'modality_id_test', 'dataset_id_test', 'MRI');
+        function testDeleteModality(testCase, modalityId, datasetId, modalityName)
+            modality = Modality(testCase.graphconn, modalityId, datasetId, modalityName);
             modality.create();
-            modality.delete('modality_id_test');
-            result = executeCypher(testCase.graphconn, 'MATCH (m:Modality {id: "modality_id_test"}) RETURN m');
+            modality.delete(modalityId);
+            result = executeCypher(testCase.graphconn, sprintf('MATCH (m:Modality {id: "%s"}) RETURN m', modalityId));
             testCase.verifyEmpty(result.m, 'Modality node was not deleted.');
         end
     end
